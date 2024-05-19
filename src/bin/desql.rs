850d5caaea728b725ec0ade2f2bd9ef36a99ab0d
xd009642 <danielmckenna93@gmail.com>
2024-05-19 16:39:11 +0100

    Start to fill in some programs ig

---
+++ src/bin/desql.rs
@@ -0,0 +1,6 @@
+//! desql is a TUI for interfacing with the database, running queries, looking at metadata etc etc.
+//! Generally all the things you'd want to do in something like psql for postgres.
+
+fn main() {
+    todo!();
+}
